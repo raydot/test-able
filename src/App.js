001af3f1c22fee1669936263d5043ad840646bd3
ItsmeRahul98 <>
2021-11-01 20:08:41 +0530

    First Commit in Design Integration
    .env data must needs to be change because we have grab that data from twillio account
    
    Design is not upto the mark but we are first working on feature than later onwards we will work on design.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import { Component } from "react";
+import { Route, Switch } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 import './App.css';
+import HomePage from "./pages/homepage/homepage";
+import TitlePage from "./pages/titlepage/titlepage";
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>testABLE</h1>
-      Online code testing for everyone.
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        {/* <h1>testABLE</h1>
+        <span>   Online code testing for everyone. </span> */}
+        <BrowserRouter>
+          <Switch>
+            <Route exact path={"/"} component={TitlePage} />
+            <Route exact path={"/visual-impairment"} component={HomePage} />
+            <Route exact path={"/hearing-impairment"} component={HomePage} />
+            <Route exact path={"/physical-impairment"} component={HomePage} />
+            <Route exact path={"/visual-hearing-impairment"} component={HomePage} />
+            <Route exact path={"/hearing-physical-impairment"} component={HomePage} />
+            <Route exact path={"/visual-physical-impairment"} component={HomePage} />
+            <Route exact path={"/visual-hearing-physical-impairment"} component={HomePage} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    )
+
+  }
 }
 
 export default App;
---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import VideoChat from "../../components/livevideo/livevideo";
+import "./homepage.scss";
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <div>
+
+            </div>
+            <div>
+                <VideoChat />
+            </div>
+            This is Homepage
+        </div>
+    );
+};
+
+export default HomePage;
