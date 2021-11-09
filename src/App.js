2b7c053094dfe27742946cc67565ee14a9b4e4e2
ItsmeRahul98 <>
2021-11-09 23:54:29 +0530

    Video mode is working as expected and remove unnecessary code and routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import { Component } from "react";
-import { Route, Switch } from "react-router";
-import { BrowserRouter } from "react-router-dom";
 import './App.css';
 import HomePage from "./pages/homepage/homepage";
-import TitlePage from "./pages/titlepage/titlepage";
 
 class App extends Component {
   render() {
@@ -11,18 +8,7 @@ class App extends Component {
       <div className='App'>
         {/* <h1>testABLE</h1>
         <span>   Online code testing for everyone. </span> */}
-        <BrowserRouter>
-          <Switch>
-            <Route exact path={"/"} component={TitlePage} />
-            <Route exact path={"/visual-impairment"} component={HomePage} />
-            <Route exact path={"/hearing-impairment"} component={HomePage} />
-            <Route exact path={"/physical-impairment"} component={HomePage} />
-            <Route exact path={"/visual-hearing-impairment"} component={HomePage} />
-            <Route exact path={"/hearing-physical-impairment"} component={HomePage} />
-            <Route exact path={"/visual-physical-impairment"} component={HomePage} />
-            <Route exact path={"/visual-hearing-physical-impairment"} component={HomePage} />
-          </Switch>
-        </BrowserRouter>
+        <HomePage />
       </div>
     )
 
